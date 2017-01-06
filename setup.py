be476723e1b35fe94e71936e2ce8f11dddafdbfa
respectus <muazs786@gmail.com>
2017-01-05 17:50:51 -0800

    Minor changes bumped to 1.0.1

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'logdna',
   packages = ['logdna'],
-  version = '1.0.0',
+  version = '1.0.1',
   description = 'A python package for sending logs to LogDNA',
   author = 'Answerbook Inc.',
   author_email = 'help@logdna.com',
   url = 'https://github.com/logdna/python',
-  download_url = 'https://github.com/logdna/python/tarball/1.0.0',
+  download_url = 'https://github.com/logdna/python/tarball/1.0.1',
   keywords = ['logdna', 'logging', 'logs', 'python', 'logdna.com', 'logger'],
   classifiers = [],
 )
