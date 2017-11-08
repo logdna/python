74b542efc5580cf881964ea4fb6ef7394562b970
respectus <muazs786@gmail.com>
2017-11-07 16:45:40 -0800

    bump to 1.2.2 ms timestamps

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'logdna',
   packages = ['logdna'],
-  version = '1.2.1',
+  version = '1.2.2',
   description = 'A python package for sending logs to LogDNA',
   author = 'Answerbook Inc.',
   author_email = 'help@logdna.com',
