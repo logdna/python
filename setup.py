ec9bb7b1918c8b795ada9aa66f8c275b1d7cc94f
respectus <muazs786@gmail.com>
2018-04-17 14:39:57 -0700

    bump version 1.2.7, support for tags

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'logdna',
   packages = ['logdna'],
-  version = '1.2.6',
+  version = '1.2.7',
   description = 'A python package for sending logs to LogDNA',
   author = 'Answerbook Inc.',
   author_email = 'help@logdna.com',
   url = 'https://github.com/logdna/python',
-  download_url = 'https://github.com/logdna/python/tarball/1.2.6',
+  download_url = 'https://github.com/logdna/python/tarball/1.2.7',
   keywords = ['logdna', 'logging', 'logs', 'python', 'logdna.com', 'logger'],
   install_requires=[
     'requests',
