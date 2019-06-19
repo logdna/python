53b15bad5816469264578b59b98c59612fd78ab1
Samir Musali <samir.musali@logdna.com>
2019-06-19 16:03:09 -0400

    1.4.0

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
 setuptools.setup(
   name = 'logdna',
   packages = ['logdna'],
-  version = '1.3.0',
+  version = '1.4.0',
   description = 'A Python Package for Sending Logs to LogDNA',
   author = 'LogDNA Inc.',
   author_email = 'help@logdna.com',
   license = 'MIT',
   url = 'https://github.com/logdna/python',
-  download_url = 'https://github.com/logdna/python/tarball/1.3.0',
+  download_url = 'https://github.com/logdna/python/tarball/1.4.0',
   keywords = ['logdna', 'logging', 'logs', 'python', 'logdna.com', 'logger'],
   install_requires=[
     'requests',
