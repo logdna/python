4230798fe2adee0df06c86c8e4603071c6187a84
Samir Musali <samir.musali@logdna.com>
2019-06-20 14:29:06 -0400

    Remove Unused Defaults

--- logdna/configs.py
+++ logdna/configs.py
@@ -1,9 +1,6 @@
 defaults = {
-    'CONTENT_TYPE': 'application/json; charset=UTF-8',
     'DEFAULT_REQUEST_TIMEOUT': 30,
-    'MS_IN_A_DAY': 24 * 60 * 60 * 1000,
     'MAX_LINE_LENGTH': 32000,
-    'MAX_INPUT_LENGTH': 32,
     'FLUSH_INTERVAL': 5,
     'FLUSH_BYTE_LIMIT': 2 * 1024 * 1024,
     'LOGDNA_URL': 'https://logs.logdna.com/logs/ingest'
