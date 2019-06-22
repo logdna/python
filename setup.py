5915698868a4fbf8fbbf2d544aacf5144abb1d87
Samir Musali <samir.musali@logdna.com>
2019-06-22 15:01:12 -0400

    1.4.2

--- setup.py
+++ setup.py
@@ -3,19 +3,19 @@ from os import path
 
 # read the contents of your README file
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+with open(path.join(this_directory, 'README.md'), 'rb') as f:
+    long_description = f.read().decode('utf-8')
 
 setup(
   name = 'logdna',
   packages = ['logdna'],
-  version = '1.4.1',
+  version = '1.4.2',
   description = 'A Python Package for Sending Logs to LogDNA',
   author = 'LogDNA Inc.',
   author_email = 'help@logdna.com',
   license = 'MIT',
   url = 'https://github.com/logdna/python',
-  download_url = 'https://github.com/logdna/python/tarball/1.4.1',
+  download_url = 'https://github.com/logdna/python/tarball/1.4.2',
   keywords = ['logdna', 'logging', 'logs', 'python', 'logdna.com', 'logger'],
   install_requires=[
     'requests',
