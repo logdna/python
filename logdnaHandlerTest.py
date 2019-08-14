ef240b2e4461648d9ee14746a5a401b08b0fa714
vilyapilya <13.veter@gmail.com>
2019-08-14 12:26:02 -0700

    use python 3 classes in the test

--- logdnaHandlerTest.py
+++ logdnaHandlerTest.py
@@ -1,4 +1,4 @@
-from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+from http.server import BaseHTTPRequestHandler,HTTPServer
 import logging
 import threading
 from logdna import LogDNAHandler
