15434f3b02f6303487b4f9b6dabc16e4a26e8f9e
vilyapilya <13.veter@gmail.com>
2019-08-26 13:36:17 -0700

    deleting the files that should've been ignored

--- build/lib/logdna/__init__.py
+++
@@ -1,8 +0,0 @@
-from .logdna import LogDNAHandler
-__all__ = ['LogDNAHandler']
-
-# Publish this class to the "logging.handlers" module so that it can be use
-# from a logging config file via logging.config.fileConfig().
-import logging.handlers
-
-logging.handlers.LogDNAHandler = LogDNAHandler
