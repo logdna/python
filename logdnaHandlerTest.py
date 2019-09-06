7f8a942993ac9a61f3e7e07e560688dd14d443f8
vilyapilya <13.veter@gmail.com>
2019-09-05 17:56:34 -0700

    rename to snake_case all var in the test file

--- logdnaHandlerTest.py
+++ logdnaHandlerTest.py
@@ -50,7 +50,6 @@ class LogDNAHandlerTest(unittest.TestCase):
           'mac': 'C0:FF:EE:C0:FF:EE'
         }
 
-        successRes = True
         server_address = ('localhost', 8081)
         httpd = HTTPServer(server_address, successful_RequestHandler)
 
@@ -61,16 +60,16 @@ class LogDNAHandlerTest(unittest.TestCase):
         def send_log():
             log.info(line)
 
-        serverThread = threading.Thread(target=httpd.handle_request)
-        logdnaThread = threading.Thread(target=send_log)
-        serverThread.daemon = True
-        logdnaThread.daemon = True
+        server_thread = threading.Thread(target=httpd.handle_request)
+        logdna_thread = threading.Thread(target=send_log)
+        server_thread.daemon = True
+        logdna_thread.daemon = True
 
-        serverThread.start()
-        logdnaThread.start()
+        server_thread.start()
+        logdna_thread.start()
 
-        serverThread.join()
-        logdnaThread.join()
+        server_thread.join()
+        logdna_thread.join()
 
         self.assertEqual(len(expectedLines), 1)
         self.assertIn(line, expectedLines)
@@ -85,24 +84,24 @@ class LogDNAHandlerTest(unittest.TestCase):
         server_address = ('localhost', 8080)
         httpd = HTTPServer(server_address, failed_RequestHandler)
 
-        failedCaseLogger = LogDNAHandler(key, options)
-        log.addHandler(failedCaseLogger)
+        failed_case_logger = LogDNAHandler(key, options)
+        log.addHandler(failed_case_logger)
         line = "second test. server fails"
 
         def send_log_to_fail():
             log.info(line)
 
-        serverThread = threading.Thread(target=httpd.handle_request)
-        logdnaThread = threading.Thread(target=send_log_to_fail)
-        serverThread.daemon = True
-        logdnaThread.daemon = True
+        server_thread = threading.Thread(target=httpd.handle_request)
+        logdna_thread = threading.Thread(target=send_log_to_fail)
+        server_thread.daemon = True
+        logdna_thread.daemon = True
 
-        serverThread.start()
-        logdnaThread.start()
+        server_thread.start()
+        logdna_thread.start()
 
-        serverThread.join()
-        logdnaThread.join()
-        self.assertEqual(len(failedCaseLogger.buf), 1)
+        server_thread.join()
+        logdna_thread.join()
+        self.assertEqual(len(failed_case_logger.buf), 1)
 
 
     def stops_retention_when_buf_is_full(self):
@@ -121,8 +120,8 @@ class LogDNAHandlerTest(unittest.TestCase):
 
         httpd = HTTPServer(server_address, failed_RequestHandler)
 
-        failedCaseLogger = LogDNAHandler(key, options)
-        log.addHandler(failedCaseLogger)
+        failed_case_logger = LogDNAHandler(key, options)
+        log.addHandler(failed_case_logger)
         line = "when buffer grows bigger than we want"
         lineTwo = "when buffer grows bigger than we want. And more and more"
 
@@ -131,19 +130,19 @@ class LogDNAHandlerTest(unittest.TestCase):
             log.info(lineTwo)
 
 
-        serverThread = threading.Thread(target=httpd.handle_request)
-        logdnaThread = threading.Thread(target=send_log_to_fail)
-        serverThread.daemon = True
-        logdnaThread.daemon = True
+        server_thread = threading.Thread(target=httpd.handle_request)
+        logdna_thread = threading.Thread(target=send_log_to_fail)
+        server_thread.daemon = True
+        logdna_thread.daemon = True
 
-        serverThread.start()
-        logdnaThread.start()
+        server_thread.start()
+        logdna_thread.start()
 
-        serverThread.join()
-        logdnaThread.join()
+        server_thread.join()
+        logdna_thread.join()
 
-        self.assertEqual(len(failedCaseLogger.buf), 1)
-        self.assertNotEqual(failedCaseLogger.buf[0]['line'], lineTwo)
+        self.assertEqual(len(failed_case_logger.buf), 1)
+        self.assertNotEqual(failed_case_logger.buf[0]['line'], lineTwo)
 
     def test_run_tests(self):
         self.server_recieves_messages()
