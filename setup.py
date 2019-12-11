6999012eb689a2393c384cd5c79685323b507242
Vilya Levitskiy <vilyalevitskiy@Vilyas-MacBook-Pro.local>
2019-12-11 15:37:06 -0800

    Don't import the package files

--- setup.py
+++ setup.py
@@ -3,21 +3,26 @@ from os import path
 
 # read the contents of your README file
 this_directory = path.abspath(path.dirname(__file__))
-import logdna._version as _version
+
+version_path = "%s/logdna/_version" % this_directory
+
 
 with open(path.join(this_directory, 'README.md'), 'rb') as f:
     long_description = f.read().decode('utf-8')
 
+with open("%s/logdna/_version.py" % this_directory) as f:
+     version = f.read().split("'")[1]
+
 setup(
   name = 'logdna',
   packages = ['logdna'],
-  version = _version.__version__,
+  version = version,
   description = 'A Python Package for Sending Logs to LogDNA',
   author = 'LogDNA Inc.',
   author_email = 'help@logdna.com',
   license = 'MIT',
   url = 'https://github.com/logdna/python',
-  download_url = ('https://github.com/logdna/python/tarball/%s' %(_version.__version__)),
+  download_url = ('https://github.com/logdna/python/tarball/%s' %(version)),
   keywords = ['logdna', 'logging', 'logs', 'python', 'logdna.com', 'logger'],
   install_requires=[
     'requests',
