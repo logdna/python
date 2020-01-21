cdf7ced9485fc5da0de2424ae3c9e893b9bb4fb7
vilyapilya <13.veter@gmail.com>
2020-01-21 12:35:18 -0800

    changes

--- logdnaHandlerTest.py
+++ logdnaHandlerTest.py
@@ -25,7 +25,6 @@ options = {
 expectedLines = []
 class successful_RequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
-
         content_length = int(self.headers['Content-Length'])
         body = self.rfile.read(content_length)
         self.send_response(200)
