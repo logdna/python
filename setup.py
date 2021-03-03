ab3632936e18c11620277d4fcced7d52cdee79b4
markcurtis1970 <markcurtis1970@gmail.com>
2021-03-03 14:48:41 +0000

    correct formatting

--- setup.py
+++ setup.py
@@ -8,8 +8,9 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), 'rb') as f:
     long_description = f.read().decode('utf-8')
 
-with open("{p}{s}logdna{s}VERSION" \
-    .format(p=this_directory, s=sep)) as f:
+with open("{p}{s}logdna{s}VERSION"
+          .format(p=this_directory, s=sep)
+          ) as f:
     version = f.read().strip('\n')
 
 setup(
