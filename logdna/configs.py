02169d6b336ffd4b0b55a52114ad152e9acbbc2c
Samir Musali <samir.musali@logdna.com>
2021-06-07 14:45:02 +0400

    refactor: reduce code complexity
    Semver: patch

--- logdna/configs.py
+++ logdna/configs.py
@@ -6,11 +6,10 @@ with open("{p}{s}VERSION".format(p=path.abspath(path.dirname(__file__)),
 
 defaults = {
     'DEFAULT_REQUEST_TIMEOUT': 30,
-    'MAX_LINE_LENGTH': 32000,
-    'FLUSH_INTERVAL_SECS': 5,
-    'FLUSH_BYTE_LIMIT': 2 * 1024 * 1024,
+    'FLUSH_INTERVAL': 250,
+    'FLUSH_LIMIT': 5 * 1024 * 1024,
     'LOGDNA_URL': 'https://logs.logdna.com/logs/ingest',
-    'BUF_RETENTION_BYTE_LIMIT': 4 * 1024 * 1024,
-    'RETRY_INTERVAL_SECS': 8,
+    'BUF_RETENTION_LIMIT': 4 * 1024 * 1024,
+    'RETRY_INTERVAL': 5000,
     'USER_AGENT': 'python/%s' % version
 }
